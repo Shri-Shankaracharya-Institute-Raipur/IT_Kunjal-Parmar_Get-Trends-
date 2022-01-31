5ff3572944266ae8cb30be1a9509852be5fe6bca
kunjal-parmar <67464846+kunjal-parmar@users.noreply.github.com>
2022-01-31 12:41:06 +0530

    Add files via upload

---
+++ build/precache-manifest.08f6b242db99096ebd49a1b476e5b0e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ca99cdf42c6835c1f94d96f83eae31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa58354a799bcdfd437",
+    "url": "/static/js/2.76480b77.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.76480b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ffd66621253e1003af",
+    "url": "/static/js/main.c02fb686.chunk.js"
+  },
+  {
+    "revision": "1c8f85eebe166863a089",
+    "url": "/static/js/runtime-main.99441741.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
